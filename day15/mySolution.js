6db155295ea35c2196a6cf9b8c3fdf92ae663b3b
Sofia Javed <sofjaved@gmail.com>
2020-12-15 13:26:54 -0500

    day15 part two solved

--- day15/mySolution.js
+++ day15/mySolution.js
@@ -2,12 +2,13 @@ let startingNumbers = [1,0,16,5,17,4];
 
 let testNumbers = [0,3,6]
 
+
 //part 1
 
-function num2020(array) {
+function targetTurn(array, target) {
   let turns = array;
   let repeats = objectify(array.slice(0, array.length-1)); //an object with all but the final starting number
-  for(let i = array.length; i < 2020; i++) { //i starts at 6, but it will be the 7th turn
+  for(let i = array.length; i < target; i++) { //i starts at 6, but it will be the 7th turn
     if(turns[i-1] in repeats) {
       turns.push(i - repeats[turns[i-1]]);
       repeats[turns[i-1]] = i; //4: turn 6
@@ -20,8 +21,8 @@ function num2020(array) {
 return turns[turns.length-1];
 }
 
-console.log(num2020(testNumbers)) // 436
-console.log(num2020(startingNumbers)) // 1294
+console.log(targetTurn(testNumbers, 2020)) // 436
+console.log(targetTurn(startingNumbers, 2020)) // 1294
 
 //part 1 helper function
 //creates an object of mentioned numbers:
@@ -36,3 +37,8 @@ function objectify(array) {
 }
 
 //part 2
+
+console.log(targetTurn(startingNumbers, 30000000)); // 573522
+
+
+
